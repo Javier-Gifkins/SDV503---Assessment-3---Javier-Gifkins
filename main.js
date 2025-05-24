72042bc48ad8936ac5c19566cbf8adcdf4f0ef94
jav <javier-gifkins@live.nmit.ac.nz>
2025-05-24 17:19:54 +1200

    Figuring Out a Settings Folder

---
+++ main.js
@@ -0,0 +1,3 @@
+const settings = require('./settings');
+
+console.log(settings.gamemode + settings.modegame);
---
+++ settings.js
@@ -0,0 +1,15 @@
+//SECTION - Settings
+
+const gamemode = "Hard"
+const modegame = "Difficult"
+
+
+
+
+
+//SECTION - Exporting Those Settings
+
+module.exports = {
+    gamemode,
+    modegame
+};
