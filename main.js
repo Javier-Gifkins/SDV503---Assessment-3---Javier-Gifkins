56c63e0a35369a3a611949ccfaa9f5e07e09281e
jav <javier-gifkins@live.nmit.ac.nz>
2025-05-24 21:36:01 +1200

    Added Password Check and Prompt for Password

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+//SECTION - Imports
 
 const fs = require("fs");
+const readline = require("readline");
+
 const path = require("path");
 const settings = require('./settings');
-
 const filePath = path.join(__dirname, "../SDV503---Assessment-3---Javier-Gifkins/userData.json");
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+})
+
+
+//SECTION - Functions
+
+function checkPassword(inputPassword) {
+    return inputPassword === settings.adminPassword;
+}
 
 function readAllPatients() {
     const data = fs.readFileSync(filePath, "utf-8");
@@ -16,16 +30,48 @@ function findPatientByID(id) {
 }
 
 
+function promptForPassword() {
+    rl.question("Enter admin password to get full access, or press enter to continue as guest: ", (input) => {
+    const isAdmin = checkPassword(input);
+
+    if (isAdmin) {
+        console.log("Admin access granted! You can CRUD patient data.");
+        let allPatients = readAllPatients();
+        console.log(allPatients);
+        rl.close();
+    } else {
+        console.log("Guest access - read-only access by patient ID.");
+        rl.question("Enter patient ID to lookup: ", (id) => {
+        let patient = findPatientByID(id);
+        if (patient) {
+            console.log("\nPatient found:", patient);
+        } else {
+            console.log("No patient found with that ID.");
+        }
+        rl.close();
+        });
+    }
+    });
+}
+
+
+
+
+
+
+
+
+
+
 
 //SECTION - Testing Section
 
+promptForPassword();
 
+// let allPatientInfo = readAllPatients();
+// console.log(allPatientInfo);
 
+// let patient = findPatientByID(settings.userLookup);
+// console.log("Found Patient: ", patient);
 
-let allPatientInfo = readAllPatients();
-console.log(allPatientInfo);
-
-let patient = findPatientByID(settings.userLookup);
-console.log("Found Patient: ", patient);
-
-console.log(settings.adminPassword);
+// console.log(settings.adminPassword);
