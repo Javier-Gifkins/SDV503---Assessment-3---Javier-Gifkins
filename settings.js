636ab900c6f5a4c637c7d8ba788bc6f838679423
jav <javier-gifkins@live.nmit.ac.nz>
2025-05-27 18:27:06 +1200

    Organised the main file. Update to the settings file.

--- settings.js
+++ settings.js
@@ -3,10 +3,32 @@
 const adminPassword = "password123"
 const userLookup = "P-XGOYBH2F"
 
+const patientInformation = [
+    { key: "name", prompt: "Name: " },
+    { key: "email", prompt: "Email: " },
+    { key: "phone", prompt: "Phone: " },
+    { key: "address", prompt: "Address: " },
+    { key: "city", prompt: "City: " },
+    { key: "country", prompt: "Country: " },
+    { key: "jobTitle", prompt: "Job Title: " },
+    { key: "birthDate", prompt: "Birth Date (yyyy/MM/DD): " },
+    { key: "bloodType", prompt: "Blood Type: " }
+];
+
+
+
+
+
+
+
+
+
+
 //SECTION - Exporting Those Settings
 
 module.exports = {
     adminPassword,
-    userLookup
+    userLookup,
+    patientInformation
     
 };
